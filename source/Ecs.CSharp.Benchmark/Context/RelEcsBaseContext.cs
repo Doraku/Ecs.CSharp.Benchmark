7d31181d81287ca72e8984aafee424c0f95bbeae
aaron.winter <aaron.winter@byterockers.games>
2022-11-03 14:08:43 +0100

    add RelEcs

---
+++ source/Ecs.CSharp.Benchmark/Context/RelEcsBaseContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RelEcs;
+
+namespace Ecs.CSharp.Benchmark.Context
+{
+    internal class RelEcsBaseContext : IDisposable
+    {
+        public class Component1
+        {
+            public int Value;
+        }
+
+        public class Component2
+        {
+            public int Value;
+        }
+
+        public class Component3
+        {
+            public int Value;
+        }
+
+        public World World { get; }
+
+        public RelEcsBaseContext()
+        {
+            World = new World();
+        }
+
+        public virtual void Dispose()
+        {
+            // World.Destroy();
+        }
+    }
+}
