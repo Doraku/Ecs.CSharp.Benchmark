d2b487d56ac3e312bc6e257a809f43b22d781988
Ullrich Praetz <ullrich@praetz.de>
2024-05-29 11:03:03 +0200

    Friflo.Engine.ECS: update to Friflo.Engine.ECS - 2.0.0-preview.3 (improved create entities performance 3x-4x)

--- source/Ecs.CSharp.Benchmark/CreateEntityWithThreeComponents/FrifloEngineEcs.cs
+++ source/Ecs.CSharp.Benchmark/CreateEntityWithThreeComponents/FrifloEngineEcs.cs
@@ -11,15 +11,9 @@ namespace Ecs.CSharp.Benchmark
         public void FrifloEngineEcs()
         {
             EntityStore store = new EntityStore(PidType.UsePidAsId);
-            store.EnsureCapacity(EntityCount);
-
             Archetype archetype = store.GetArchetype(ComponentTypes.Get<Component1, Component2, Component3>());
-            archetype.EnsureCapacity(EntityCount);
-
-            for (int i = 0; i < EntityCount; ++i)
-            {
-                archetype.CreateEntity();
-            }
+            // returns an enumerator of created entities
+            archetype.CreateEntities(EntityCount);
         }
     }
 }
